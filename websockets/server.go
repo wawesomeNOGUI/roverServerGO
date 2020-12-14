c274254c1a4ec5c1fdb74beaf2bc0bbca6c731cb
wawesomeNOGUI <67801725+wawesomeNOGUI@users.noreply.github.com>
2020-12-14 16:51:32 -0500

    Add files via upload

--- websockets/server.go
+++ websockets/server.go
@@ -43,7 +43,7 @@ import (
 */
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addr = flag.String("addr", ":8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options for upgrader
 
