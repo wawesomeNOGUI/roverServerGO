af9812410bf8bb2c53a4aff3e022986f3380dc40
wawesomeNOGUI <67801725+wawesomeNOGUI@users.noreply.github.com>
2020-12-23 17:15:35 -0500

    Add files via upload

--- roverWebrtc/v3test.go
+++ roverWebrtc/v3test.go
@@ -1,9 +1,3 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// +build ignore
-
 package main
 
 import (
@@ -110,7 +104,7 @@ func echo(w http.ResponseWriter, r *http.Request) {
                         s, err := mediadevices.GetUserMedia(mediadevices.MediaStreamConstraints{
                                 Video: func(c *mediadevices.MediaTrackConstraints) {
                                         c.FrameFormat = prop.FrameFormat(frame.FormatYUY2)
-                                        //c.FrameFormat = prop.FrameFormat(frame.FormatI420)
+                                        //c.FrameFormat = prop.FrameFormatExact(frame.FormatI420)
                                         c.Width = prop.Int(352)
                                         c.Height = prop.Int(288)
                                 },
@@ -163,7 +157,7 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
                         dt := time.Now()
                         log.Print(dt.String())
-                        //Wait for ICE gathering to find the best candidate for the LocalDescription(non-trickle ICE)
+                        //Wait for ICE gathering to complete (non-trickle ICE)
                         <-gatherComplete
 
                         dt = time.Now()
@@ -173,16 +167,13 @@ func echo(w http.ResponseWriter, r *http.Request) {
                         //Output the SDP with the final ICE candidate
                         log.Println( signal.Encode(*peerConnection.LocalDescription()) )
 
-                        //write offer back to browser through the websocket connection
-                        err = c.WriteMessage( 1, []byte( signal.Encode(*peerConnection.LocalDescription()) ) )
+                        err = c.WriteMessage( 1, []byte( signal.Encode(*peerConnection.LocalDescription()) ) )  //write message back to browser
                                 if err != nil {
                                   log.Println("write:", err)
                                 }
 
 
-                        //Wait for browser's answer SDP
-                       //ReadMessage blocks until message received
-                 msgType, message, err2 := c.ReadMessage()
+                 msgType, message, err2 := c.ReadMessage()   //ReadMessage blocks until message received
                         if err2 != nil {
                                 log.Println("read:", err)
                                 //break
@@ -192,7 +183,7 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
                 answer := webrtc.SessionDescription{}
                         //signal.Decode(signal.MustReadStdin(), &offer)
-                        signal.Decode(SDP, &answer)   //set answer to the decoded SDP
+                        signal.Decode(SDP, &answer)   //set offer to the decoded SDP
                         log.Print(answer)
 
                 // Set the remote SessionDescription
@@ -243,85 +234,3 @@ func main() {
 
         //wg.Wait()  //the main function won't exit until httpServerAndWebsockets is exited
 }
-
-
-/*
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>
-window.addEventListener("load", function(evt) {
-
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.textContent = message;
-        output.appendChild(d);
-    };
-
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server,
-"Send" to send a message to the server and "Close" to close the connection.
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output"></div>
-</td></tr></table>
-</body>
-</html>
-`))
-*/
